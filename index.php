369be751f6e64f7c29fafe77b83ef219dd9927ac
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2018-11-14 06:21:04 +0700

    Update header code and fix login page

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Berita Seputar Putaran</title>
-	<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
-	<script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="asset/js/jquery.min.js"></script>
-	
-	<link rel="stylesheet" type="text/css" href="asset/css/style.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <?php include_once 'head_content.php' ?>
 </head>
 <body>
 <!--
